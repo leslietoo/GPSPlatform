a2134f28fecb4ae8bc4338355be19f6fac371415
SmallChi <564952747@qq.com>
2018-08-11 16:34:24 +0800

    JT808分发器实现（具体业务需具体实现）

---
+++ src/GPS.Dispatchers/GPS.JT808DeviceMonitoringDispatcher/JT808DeviceMonitoringDispatcherImpl.cs
@@ -0,0 +1,16 @@
+﻿using GPS.Dispatcher.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPS.JT808DeviceMonitoringDispatcher
+{
+    public class JT808DeviceMonitoringDispatcherImpl : IDeviceMonitoringDispatcher
+    {
+        public async Task SendAsync(string deviceKey, byte[] data)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ src/GPS.Dispatchers/GPS.JT808SourcePackageDispatcher/JT808SourcePackageDispatcherImpl.cs
@@ -0,0 +1,16 @@
+﻿using GPS.Dispatcher.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPS.JT808SourcePackageDispatcher
+{
+    public class JT808SourcePackageDispatcherImpl : ISourcePackageDispatcher
+    {
+        public async Task SendAsync(byte[] data)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
