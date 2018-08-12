aa74d66fae7715c026df1911157f3bde573b1d07
SmallChi <564952747@qq.com>
2018-08-12 22:38:51 +0800

    增加具体包的实现

--- src/JT808.Protocol/JT808Serializer.cs
+++ src/JT808.Protocol/JT808Serializer.cs
@@ -40,6 +40,57 @@ namespace JT808.Protocol
             }
         }
 
+        public static byte[] Serialize(JT808Package jT808Package)
+        {
+            return Serialize(jT808Package, defaultResolver);
+        }
+
+        public static byte[] Serialize(JT808Package jT808Package, IJT808FormatterResolver resolver)
+        {
+            if (resolver == null) resolver = DefaultResolver;
+            var formatter = resolver.GetFormatter<JT808Package>();
+            // ref https://www.cnblogs.com/TianFang/p/9193881.html
+            var pool = ArrayPool<byte>.Shared;
+            byte[] buffer = pool.Rent(65536);
+            try
+            {
+                var len = formatter.Serialize(ref buffer, 0, jT808Package, DefaultResolver);
+                return buffer.AsSpan().Slice(0, len).ToArray();
+            }
+            catch (JT808Exception ex)
+            {
+                throw new JT808Exception("Serialize", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Serialize", ex);
+            }
+            finally
+            {
+                pool.Return(buffer, true);
+            }
+        }
+
+        public static JT808Package Deserialize(ReadOnlySpan<byte> bytes, IJT808FormatterResolver resolver)
+        {
+            try
+            {
+                if (resolver == null) resolver = DefaultResolver;
+                var formatter = resolver.GetFormatter<JT808Package>();
+                int readSize;
+                return formatter.Deserialize(bytes, 0, resolver, out readSize);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Deserialize", ex);
+            }
+        }
+
+        public static JT808Package Deserialize(ReadOnlySpan<byte> bytes)
+        {
+            return Deserialize(bytes, defaultResolver);
+        }
+
         public static byte[] Serialize<T>(T obj)
         {
             return Serialize(obj, defaultResolver);
