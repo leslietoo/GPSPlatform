a83a6d39434aacebb2b29760f477cdd2dd741ea8
SmallChi <564952747@qq.com>
2019-01-09 10:59:46 +0800

    1.增加808消息处理接口 2.修改消费者接口去掉消息id参数

---
+++ src/GPS.Abstractions/IJT808MsgIdHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPS.Abstractions
+{
+    /// <summary>
+    /// JT808消息Id处理程序
+    /// </summary>
+    public interface IJT808MsgIdHandler
+    {
+        void Processor(byte[] data);
+    }
+}
--- src/GPS.PubSub.Abstractions/IJT808Consumer.cs
+++ src/GPS.PubSub.Abstractions/IJT808Consumer.cs
@@ -7,7 +7,7 @@ namespace GPS.PubSub.Abstractions
 {
     public interface IJT808Consumer : IJT808PubSub, IDisposable
     {
-        void OnMessage(string msgId, Action<(string MsgId, byte[] data)> callback);
+        void OnMessage(Action<(string MsgId, byte[] data)> callback);
         CancellationTokenSource Cts { get; }
         void Subscribe();
         void Unsubscribe();
