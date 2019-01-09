532b686d9ab6033afd865545ab340a5a6bc4b3ef
TK <qqcc2012game@163.com>
2019-01-09 11:30:17 +0800

    修改原包转发类的继承接口

---
+++ src/GPS.JT808.Gateway/MsgIdsHandlers/JT808MsgIdTcpCustomHandler.cs
@@ -0,0 +1,135 @@
+﻿using GPS.PubSub.Abstractions;
+using JT808.DotNetty.Core;
+using JT808.DotNetty.Core.Handlers;
+using JT808.DotNetty.Core.Metadata;
+using JT808.Protocol.Enums;
+using JT808.Protocol.Extensions;
+using JT808.Protocol.MessageBody;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPS.JT808.Gateway.MsgIdsHandlers
+{
+    public class JT808MsgIdTcpCustomHandler : JT808MsgIdTcpHandlerBase
+    {
+        private readonly IJT808Producer jT808Producer;
+        private readonly ILogger<JT808MsgIdTcpCustomHandler> logger;
+
+        public JT808MsgIdTcpCustomHandler(
+            IJT808Producer jT808Producer,
+            ILoggerFactory loggerFactory,
+            JT808TcpSessionManager sessionManager ) : base(sessionManager)
+        {
+            logger = loggerFactory.CreateLogger<JT808MsgIdTcpCustomHandler>();
+            this.jT808Producer = jT808Producer;
+            logger.LogDebug("test");
+        }
+        public override JT808Response Msg0x0001(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0001(request);
+        }
+        public override JT808Response Msg0x0002(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0002(request);
+        }
+        public override JT808Response Msg0x0003(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0003(request);
+        }
+        public override JT808Response Msg0x0100(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0100(request);
+        }
+        public override JT808Response Msg0x0102(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0102(request);
+        }
+        public override JT808Response Msg0x0200(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0200(request);
+        }
+        public override JT808Response Msg0x0704(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0704(request);
+        }
+        public override JT808Response Msg0x0900(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return base.Msg0x0900(request);
+        }
+        public JT808Response Msg0x0705(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return new JT808Response(JT808MsgId.平台通用应答.Create(request.Package.Header.TerminalPhoneNo, new JT808_0x8001()
+            {
+                MsgId = request.Package.Header.MsgId,
+                JT808PlatformResult = JT808PlatformResult.成功,
+                MsgNum = request.Package.Header.MsgNum
+            }));
+        }
+        public JT808Response Msg0x0801(JT808Request request)
+        {
+            if (logger.IsEnabled(LogLevel.Debug))
+            {
+                logger.LogDebug(string.Join(" ", request.OriginalPackage));
+                logger.LogDebug(Newtonsoft.Json.JsonConvert.SerializeObject(request.Package));
+            }
+            jT808Producer.ProduceAsync(request.Package.Header.MsgId.ToString(), request.Package.Header.TerminalPhoneNo, request.OriginalPackage);
+            return null ;
+        }
+    }
+}
---
+++ src/GPS.JT808.Gateway/Program.cs
@@ -0,0 +1,75 @@
+﻿using Confluent.Kafka;
+using GPS.JT808.Gateway.MsgIdsHandlers;
+using GPS.JT808PubSubToKafka;
+using GPS.JT808SourcePackageDispatcher;
+using GPS.JT808SourcePackageDispatcher.Configs;
+using GPS.PubSub.Abstractions;
+using JT808.DotNetty.Abstractions;
+using JT808.DotNetty.Core;
+using JT808.DotNetty.Core.Handlers;
+using JT808.DotNetty.Tcp;
+using JT808.DotNetty.Udp;
+using JT808.DotNetty.WebApi;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace GPS.JT808.Gateway
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+               var serverHostBuilder = new HostBuilder()
+               .UseEnvironment(args[0].Split('=')[1])
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(AppDomain.CurrentDomain.BaseDirectory);
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    config.AddJsonFile($"appsettings.{ hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureLogging((context, logging) =>
+                {
+                    if (Environment.OSVersion.Platform == PlatformID.Unix)
+                    {
+                        NLog.LogManager.LoadConfiguration("Configs/nlog.unix.config");
+                    }
+                    else {
+                        NLog.LogManager.LoadConfiguration("Configs/nlog.win.config");
+                    }
+                    logging.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<ILoggerFactory, LoggerFactory>();
+                    services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+                    services.Configure<RemoteServerOptions>(hostContext.Configuration.GetSection("RemoteServerOptions"));
+                    services.Configure<ProducerConfig>(hostContext.Configuration.GetSection("KafkaProducerConfig"));
+                    services.Configure<ConsumerConfig>(hostContext.Configuration.GetSection("KafkaConsumerConfig"));
+                    services.AddSingleton<IJT808Producer, JT808_MsgId_Producer>();
+
+                    // 自定义Tcp消息处理业务
+                    services.Replace(new ServiceDescriptor(typeof(JT808MsgIdTcpHandlerBase), typeof(JT808MsgIdTcpCustomHandler), ServiceLifetime.Singleton));
+                    // 自定义Udp消息处理业务
+                    services.Replace(new ServiceDescriptor(typeof(JT808MsgIdUdpHandlerBase), typeof(JT808MsgIdUdpCustomHandler), ServiceLifetime.Singleton));
+                    
+                    services.Replace(new ServiceDescriptor(typeof(IJT808SessionPublishing), typeof(JT808_SessionPublishing_Producer), ServiceLifetime.Singleton));
+                    //原包转发
+                    services.Replace(new ServiceDescriptor(typeof(IJT808SourcePackageDispatcher), typeof(JT808SourcePackageDispatcherImpl), ServiceLifetime.Singleton));
+
+                    services.AddJT808Core(hostContext.Configuration)
+                    .AddJT808TcpHost()
+                    .AddJT808UdpHost()
+                    .AddJT808WebApiHost();
+                });
+
+            await serverHostBuilder.RunConsoleAsync();
+        }
+    }
+}
